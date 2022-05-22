1a494492c7c3ba59f3bcf2918507c0eed14f46a3
deege <dj@deege.com>
2022-05-22 16:20:38 -0600

    Updated animations to face player in movement direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,21 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 moveInput;
     Rigidbody2D rigidbody2d;
+    Animator animator;
 
-    [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float moveSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateAnimation();
         Run();
     }
 
@@ -30,5 +33,12 @@ public class PlayerMovement : MonoBehaviour
 
     void Run() {
         rigidbody2d.velocity = moveInput * moveSpeed ;
+    }
+
+    void UpdateAnimation() {
+        animator.SetBool("isMovingUp", moveInput.y > 0);
+        animator.SetBool("isMovingDown", moveInput.y < 0);
+        animator.SetBool("isMovingLeft", moveInput.x < 0);
+        animator.SetBool("isMovingRight", moveInput.x > 0);
     }
 }
