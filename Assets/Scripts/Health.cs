ff736a307d7f275aab9b2d7e803a72ea1646e072
deege <dj@deege.com>
2022-05-24 16:51:51 -0600

    Added damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+   [SerializeField] int health = 50;
+
+   void OnTriggerEnter2D(Collider2D other) {
+       DamageDealer damage = other.GetComponent<DamageDealer>();
+       if (damage != null)
+        {
+            if (gameObject.tag == other.tag) {
+                // skip
+            } else {
+                TakeDamage(damage.getDamage());
+                damage.Hit();
+            }
+        }
+    }
+
+    void TakeDamage(int damage) {
+        health -= damage;
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
